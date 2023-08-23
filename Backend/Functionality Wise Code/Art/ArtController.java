080c027ccc36b786e403f8804bd01f65c3aab8f3
Mahendra <mahendrakumar27697@gmail.com>
2023-08-23 21:35:48 +0530

    .

--- Backend/Functionality Wise Code/Art/ArtController.java
+++ Backend/Functionality Wise Code/Art/ArtController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entities.Art;
 import com.example.demo.entities.ArtAdd;
@@ -30,10 +32,24 @@ public class ArtController {
 	@PostMapping("/addart")
 	public Art addArtImage(@RequestBody ArtAdd o)
 	{
-		Art a=new Art(o.getArt_id(),o.getCat_id(),o.getPrice(),o.getNgo_id(),o.getDescription(),o.getArt_name(),o.getImage());
+		Art a=new Art(o.getArt_id(),o.getCat_id(),o.getPrice(),o.getNgo_id(),o.getDescription(),o.getArt_name());
 		Art saved=aservice.addArt(a);
 		
 		return saved;
 	}
-
+	
+	@PostMapping(value="/uploadimage/{art_id}",consumes = "multipart/form-data")
+	public boolean uploadArt(@PathVariable int art_id,@RequestBody MultipartFile image)
+	{
+		boolean flag=true;
+		try
+		{
+			flag=aservice.uploadImage(art_id, image.getBytes());
+		}
+		catch(Exception e)
+		{
+			flag=false;
+		}
+		return flag;
+	}
 }
---
+++ Backend/Functionality Wise Code/Ngo Registration/Ngo.java
@@ -0,0 +1,165 @@
+package com.example.demo.entities;
+
+import java.sql.Blob;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="ngo")
+public class Ngo {
+	
+	@Id
+	@GeneratedValue(strategy =GenerationType.IDENTITY)
+	private int ngo_id;
+	
+	@JsonIgnoreProperties("Login")
+	@OneToOne
+	@JoinColumn(name="user_id")
+	Login user_id;
+	@Column
+	private String ngo_name;
+	@Column
+	private String domain;
+	@Column
+	private int area_id;
+	
+	@Column
+	private String address;
+	@Column
+	private String contact;
+	@Column
+	private byte[] certificate;
+	@Column
+	private String account_no;
+	
+	
+	
+
+
+	public Ngo( String ngo_name, String domain, int area_id, String address, String contact
+			, String account_no, Login user_id) {
+		super();
+		
+		this.user_id = user_id;
+		this.ngo_name = ngo_name;
+		this.domain = domain;
+		this.area_id = area_id;
+		this.address = address;
+		this.contact = contact;
+		this.certificate = certificate;
+		this.account_no = account_no;
+	}
+	
+
+
+
+
+	public int getNgo_id() {
+		return ngo_id;
+	}
+
+
+	public void setNgo_id(int ngo_id) {
+		this.ngo_id = ngo_id;
+	}
+
+
+	public Login getUser_id() {
+		return user_id;
+	}
+
+
+	public void setUser_id(Login user_id) {
+		this.user_id = user_id;
+	}
+
+
+	public String getNgo_name() {
+		return ngo_name;
+	}
+
+
+	public void setNgo_name(String ngo_name) {
+		this.ngo_name = ngo_name;
+	}
+
+
+	public String getDomain() {
+		return domain;
+	}
+
+
+	public void setLname(String domain) {
+		this.domain = domain;
+	}
+
+
+	public int getArea_id() {
+		return area_id;
+	}
+
+
+	public void setArea_id(int area_id) {
+		this.area_id = area_id;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getContact() {
+		return contact;
+	}
+
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+
+	public byte[] getCertificate() {
+		return certificate;
+	}
+
+
+	public void setCertificate( byte[] certificate) {
+		this.certificate = certificate;
+	}
+
+
+	public String getAccount_no() {
+		return account_no;
+	}
+
+
+	public void setAccount_no(String account_no) {
+		this.account_no = account_no;
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+
+}
