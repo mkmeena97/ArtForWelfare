c8aff906649f4647b2c285a966091752398eb34f
Nikhil <ntalmale07@gmail.com>
2023-08-25 10:45:13 +0530

    home

--- Frontend/AfwReact/src/components/NgoHome.js
+++ Frontend/AfwReact/src/components/NgoHome.js
@@ -1,26 +1,29 @@
-import { useEffect } from "react"
+import { useEffect,useState } from "react"
+
+import {Link,Route,Routes} from "react-router-dom"
 
 export default function NgoHome(){
-    const[artist,setArtist]=useState(null);
+    const[ngo,setNgo]=useState(null);
 
     
     useEffect(()=>{
        const userid = JSON.parse(localStorage.getItem("loggeduser")).user_id;   
 
-        fetch("http://localhost:8080/"+userid)
+        fetch("http://localhost:8080/getNGO"+userid)
         .then(resp=>resp.json())
         .then(obj=>{
-            localStorage.setItem("loggedartist",JSON.stringify(obj))
-            setArtist(obj);
+            localStorage.setItem("loggedngo",JSON.stringify(obj))
+            setNgo(obj);
         })
     },[])
+
 
     return(
         <div>
             
-             <p>Welcome {ngo && ngo.fname} {artist && artist.lname}</p>
+             <p>Welcome {ngo && ngo.fname}</p>
             <h1>Artist's Home Page</h1>
-            <Link to="/" >View Fund</Link>
+            <Link to="/" >View Fund By Artist</Link>
             <Link to="/" >view Artist</Link>
             <Link to="/" >Total fund Received</Link>
 
