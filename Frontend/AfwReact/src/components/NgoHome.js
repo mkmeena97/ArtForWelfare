318bf9d59a7ad310d68be682df4f731a7abc7ea0
Nikhil <ntalmale07@gmail.com>
2023-08-25 10:12:09 +0530

    ngo

--- Frontend/AfwReact/src/components/NgoHome.js
+++ Frontend/AfwReact/src/components/NgoHome.js
@@ -1,7 +1,30 @@
+import { useEffect } from "react"
+
 export default function NgoHome(){
+    const[artist,setArtist]=useState(null);
+
+    
+    useEffect(()=>{
+       const userid = JSON.parse(localStorage.getItem("loggeduser")).user_id;   
+
+        fetch("http://localhost:8080/"+userid)
+        .then(resp=>resp.json())
+        .then(obj=>{
+            localStorage.setItem("loggedartist",JSON.stringify(obj))
+            setArtist(obj);
+        })
+    },[])
+}
+
     return(
         <div>
-            <h1>NGO Home Page</h1>
+            
+             <p>Welcome {ngo && ngo.fname} {artist && artist.lname}</p>
+            <h1>Artist's Home Page</h1>
+            <Link to="/" >View Fund</Link>
+            <Link to="/" >view Artist</Link>
+            <Link to="/" >Total fund Received</Link>
+
         </div>
         
     )
