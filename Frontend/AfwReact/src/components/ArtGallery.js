4929c5c1adf4f391b16927b157742a711414d421
Tushar <dhanorkartushar2@gmail.com>
2023-08-27 09:25:53 +0530

    about and contact us

--- Frontend/AfwReact/src/components/ArtGallery.js
+++ Frontend/AfwReact/src/components/ArtGallery.js
@@ -1,10 +1,10 @@
+
 import React, { useState, useEffect } from 'react';
-import './ArtGallery.css';
-import Cart from './Cart';
+import './ArtGallery.css'; // Import your CSS file if needed
 
 const ArtGallery = () => {
   const [artList, setArtList] = useState([]);
-  const [cart, setCart] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,62 +21,38 @@ const ArtGallery = () => {
     };
 
     fetchData();
+    const storedCartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    setCartItems(storedCartItems);
   }, []);
 
   useEffect(() => {
-    // Load cart from local storage
-    const savedCart = localStorage.getItem('cart');
-    if (savedCart) {
-      setCart(JSON.parse(savedCart));
-    }
+    const storedCartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    setCartItems(storedCartItems);
   }, []);
 
-  const updateLocalStorageCart = (updatedCart) => {
-    localStorage.setItem('cart', JSON.stringify(updatedCart));
-  };
-
   const addToCart = (artPiece) => {
-    if (!cart.some(item => item.art_id === artPiece.art_id)) {
-      const updatedCart = [...cart, artPiece];
-      setCart(updatedCart);
-      // Save updated cart to local storage
-      updateLocalStorageCart(updatedCart);
-    }
-  };
-
-  const removeFromCart = (artPiece) => {
-    const updatedCart = cart.filter(item => item.art_id !== artPiece.art_id);
-    setCart(updatedCart);
-    // Save updated cart to local storage
-    updateLocalStorageCart(updatedCart);
+    const updatedCartItems = [...cartItems, artPiece];
+    localStorage.setItem('cartItems', JSON.stringify(updatedCartItems));
+    setCartItems(updatedCartItems);
   };
 
   return (
     <div className="art-gallery">
       {artList.map((artPiece) => (
         <div key={artPiece.art_id} className="art-card">
-          <img src={`data:image/jpeg;base64,${artPiece.image}`} alt={artPiece.title} />
+          <img src={`data:image/jpeg;base64,${artPiece.image}`} alt={artPiece.title} className='art-image' />
           <h3>{artPiece.art_name}</h3>
           <p>Description: {artPiece.description}</p>
           <p>Artist: {artPiece.artist_name}</p>
           <p>Price: {artPiece.price}</p>
-          {cart.some(item => item.art_id === artPiece.art_id) ? (
-            <button className="btn btn-secondary" disabled>
-              Already in Cart
-            </button>
-          ) : (
-            <button
-              className="btn btn-success"
-              onClick={() => addToCart(artPiece)}
-            >
-              Add to Cart
-            </button>
-          )}
+          <button
+            onClick={() => addToCart(artPiece)}
+            disabled={cartItems.some((item) => item.art_id === artPiece.art_id)}
+          >
+            {cartItems.some((item) => item.art_id === artPiece.art_id) ? 'Already in Cart' : 'Add to Cart'}
+          </button>
         </div>
       ))}
-      <div className="cart-container">
-        <Cart cart={cart} removeFromCart={removeFromCart} />
-      </div>
     </div>
   );
 };
